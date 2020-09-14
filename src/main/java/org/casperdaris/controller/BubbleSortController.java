2b3dc9a7808e540d4e9514194534ece2a124f31e
Casper Daris <casperdaris@msn.com>
2020-09-14 17:37:36 +0200

    Int Array vervangen door ArrayList, gebruiker heeft mogenlijkheid tot toevoegen van Integers

--- src/main/java/org/casperdaris/controller/BubbleSortController.java
+++ src/main/java/org/casperdaris/controller/BubbleSortController.java
@@ -1,38 +1,33 @@
 package org.casperdaris.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BubbleSortController {
 
-    public static int[] intArray = new int[] {4, 32, 534, 21, 5, 12, 1, 43, 63, 643, 322, 23, 41, 739, 301, 4321, 74, 2};
+    public static ArrayList<Integer> intLijst = new ArrayList<Integer>();
 
-    public static void Sorteer(int[] array) {
+    public static void Sorteer(ArrayList<Integer> list) {
 
-        // Deze Integer is de laatste ongesorteerde Int in de Array
-        int lastUnsorted = array.length - 1;
-
-        // Deze for loop zorgt ervoor dat de Integers vergeleken blijven worden totdat de lastUnsorted Integer is bereikt
-        for (int i = 0; i < lastUnsorted; i++) {
+        // Deze for loop zorgt ervoor dat de Integers vergeleken blijven worden totdat alle Integers vergeleken zijn
+        for (int i = 0; i < list.size() - 1; i++) {
 
             // Vergelijken welke Integer de grootste waarde heeft
-            if (array[i] > array[i + 1]) {
+            if (list.get(i) > list.get(i + 1)) {
 
                 // Wanneer Integer i groter is dan de volgende Integer, dan omwisselen van plaats
-                Wissel(array, i, i + 1);
+                Wissel(list, i, i + 1);
             }
         }
 
-        // Zodra de for loop is afgelopen, de laatste ongesorteerde Int veranderen
-        lastUnsorted--;
-
-        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(intLijst.toArray()));
     }
 
     // Deze functie verwisseld simpelweg twee Integers in een Array
-    public static void Wissel(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    public static void Wissel(ArrayList<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(i + 1));
+        list.set(j, temp);
     }
 
 }
--- src/main/java/org/casperdaris/view/BubbleSortView.java
+++ src/main/java/org/casperdaris/view/BubbleSortView.java
@@ -1,21 +1,56 @@
 package org.casperdaris.view;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import org.casperdaris.controller.BubbleSortController;
 
 public class BubbleSortView {
 
-    private Button knop;
+    private Button eenmaligUitvoerenKnop, alleUitvoerenKnop, cijferToevoegenKnop, lijstLeegmakenKnop, printLijstKnop;
+    private TextField invoerVeld;
 
     public BubbleSortView(GridPane view) {
 
-        knop = new Button("Uitvoeren");
-        knop.setOnAction(event -> {
-            BubbleSortController.Sorteer(BubbleSortController.intArray);
+        invoerVeld = new TextField();
+        invoerVeld.setPrefWidth(0.3);
+
+        cijferToevoegenKnop = new Button("Toevoegen");
+        cijferToevoegenKnop.setOnAction(event -> {
+            if (!invoerVeld.getText().isEmpty()) {
+                BubbleSortController.intLijst.add(Integer.parseInt(invoerVeld.getText()));
+                invoerVeld.clear();
+            }
         });
 
+        lijstLeegmakenKnop = new Button("Leegmaken");
+        lijstLeegmakenKnop.setOnAction(event -> {
+            BubbleSortController.intLijst.clear();
+            System.out.println("Lijst opgeschoond");
+        });
+
+        printLijstKnop = new Button("Print");
+        printLijstKnop.setOnAction(event -> {
+            System.out.println(BubbleSortController.intLijst);
+        });
+
+        eenmaligUitvoerenKnop = new Button("Eenmalig");
+        eenmaligUitvoerenKnop.setOnAction(event -> {
+            BubbleSortController.Sorteer(BubbleSortController.intLijst);
+        });
+
+        alleUitvoerenKnop = new Button("Voltooien");
+
         view.setPrefSize(360, 240);
-        view.add(knop, 0, 0);
+        view.add(invoerVeld, 0, 0);
+        view.add(cijferToevoegenKnop, 1, 0);
+        view.add(lijstLeegmakenKnop, 2, 0);
+        view.add(printLijstKnop, 0, 1);
+        view.add(eenmaligUitvoerenKnop, 1, 1);
+        view.add(alleUitvoerenKnop, 2, 1);
+        view.setHgap(12);
+        view.setVgap(12);
+        view.setPadding(new Insets(20, 20, 20, 20));
     }
 }
